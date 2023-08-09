9a6bfc83e059a5a44bb7d2431b7d6331627dd571
Ana Bastos <102618213+Anabastoslps@users.noreply.github.com>
2023-08-09 11:44:13 -0300

    Update index.php

--- calculartempo/index.php
+++ calculartempo/index.php
@@ -8,32 +8,36 @@
         <h2>Calcular tempo entre duas datas</h2>
 
         <?php
-        
-        $entrada = new DateTime("2020-02-19 09:00:00");
+        $entrada = new DateTime("2022-02-19 09:00:00");
         $saida = new DateTime("2023-08-08 09:00:00");
         
-        echo "Entrada: " . $entrada->format('d/m/y H:i:s') . "<br>";
+        echo "Entrada: " . $entrada->format('d/m/Y H:i:s') . "<br>";
         echo "Saida: " . $saida->format('d/m/y H:i:s') . "<br><br>";
         
         //calcular o intervalo entre as datas
         $intervalo = $entrada->diff($saida);
-        
-        var_dump($intervalo);
+        //var_dump($intervalo);
 
         //calcular o tempo entre as duas datas
-        echo $intervalo->y. "anos," . $intervalo->m . " meses," . $intervalo->d . "dias. <br>";
+        echo $intervalo->y . " anos, " . $intervalo->m . " meses, " . $intervalo->d . " dias. <br>";
 
-        //Total de dias
-        echo $intervalo->days . " dias. <br>";
+        //total de dias
+        echo $intervalo->days . " dias. <br><br>";
+
+        $entrada_b = new DateTime("2022-02-19 09:00:00");
+        $saida_b = new DateTime("2023-08-08 09:00:00");
+        
+        echo "Entrada: " . $entrada_b->format('d/m/Y H:i:s') . "<br>";
+        echo "Saida: " . $saida_b->format('d/m/y H:i:s') . "<br><br>";
 
         //obter o intervalo em segundos
-        echo $saida->getTimestamp() - $entrada->getTimestamp() . " segundos. <br>";
+        echo $saida_b->getTimestamp() - $entrada->getTimestamp() . " segundos. <br>";
 
         //obter o intervalo em minutos
-        echo ($saida->getTimestamp() - $entrada->getTimestamp())/60 . " minutos. <br>";
+        echo ($saida_b->getTimestamp() - $entrada->getTimestamp())/60 . " minutos. <br>";
 
          //obter o intervalo em horas
-         echo (($saida->getTimestamp() - $entrada->getTimestamp())/60) / 60 . " horas. <br>";
+         echo (($saida_b->getTimestamp() - $entrada->getTimestamp())/60) / 60 . " horas. <br>";
         ?>
     </body>
 </html>
